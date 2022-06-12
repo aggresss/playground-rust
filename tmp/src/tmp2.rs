3c97f9766f753543665451fc93aae38f026bbe77
aggresss <aggresss@163.com>
2022-06-12 22:19:07 +0800

    2022-06-12 22:19:07 CST W24D0

--- tmp/src/tmp2.rs
+++ tmp/src/tmp2.rs
@@ -1,3 +1,10 @@
-fn main() {
+fn print_type_of<T>(_: &T) {
+    println!("{}", std::any::type_name::<T>());
+}
 
+fn main() {
+    let x: (u32, u16) = (1, 2);
+    print_type_of(&x);
+    print_type_of(&{});
+    print_type_of(&());
 }
--- workbench/tokio-demo/src/client.rs
+++ workbench/tokio-demo/src/client.rs
@@ -1,11 +1,17 @@
-async fn say_world() {
-    println!("world");
-}
+use mini_redis::{client, Result};
 
 #[tokio::main]
-async fn main() {
-    let op = say_world();
-    println!("hello");
+async fn main() -> Result<()> {
+    // Open a connection to the mini-redis address.
+    let mut client = client::connect("127.0.0.1:6379").await?;
 
-    op.await;
+    // Set the key "hello" with value "world"
+    client.set("hello", "world".into()).await?;
+
+    // Get key "hello"
+    let result = client.get("hello").await?;
+
+    println!("got value from the server; result={:?}", result);
+
+    Ok({})
 }
