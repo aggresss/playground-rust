cd150ab1b125b14fc4bd3c7a31daa4514671fc22
aggresss <aggresss@163.com>
2022-06-13 22:16:25 +0800

    2022-06-13 22:16:25 CST W24D1

--- workbench/tokio-demo/src/client.rs
+++ workbench/tokio-demo/src/client.rs
@@ -3,7 +3,7 @@ use mini_redis::{client, Result};
 #[tokio::main]
 async fn main() -> Result<()> {
     // Open a connection to the mini-redis address.
-    let mut client = client::connect("127.0.0.1:6379").await?;
+    let mut client = client::connect("127.0.0.1:63790").await?;
 
     // Set the key "hello" with value "world"
     client.set("hello", "world".into()).await?;
@@ -13,5 +13,5 @@ async fn main() -> Result<()> {
 
     println!("got value from the server; result={:?}", result);
 
-    Ok({})
+    Ok(())
 }
