3bf2b31840d8303b83c9826d45c8a23f941d7d1c
aggresss <aggresss@163.com>
2022-06-18 11:21:27 +0800

    2022-06-18 11:21:27 CST W24D6

--- tmp/examples/tmp3.rs
+++ tmp/examples/tmp3.rs
@@ -13,17 +13,28 @@ fn main() {
     }
 }
 
-
 fn handle_connection(mut stream: TcpStream) {
     let mut buffer = [0; 1024];
-
     stream.read(&mut buffer).unwrap();
 
     println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
 
-    let contents = fs::read_to_string("assets/hello.html").unwrap();
+    let get = b"GET / HTTP/1.1\r\n";
 
-    let response = format!("HTTP/1.1 200 OK\r\nContent-Length: {}\r\n\r\n{}", contents.len(), contents);
+    let (status_line, filename) = if buffer.starts_with(get) {
+        ("HTTP/1.1 200 OK", "assets/hello.html")
+    } else {
+        ("HTTP/1.1 404 NOT FOUND", "assets/404.html")
+    };
+
+    let contents = fs::read_to_string(filename).unwrap();
+
+    let response = format!(
+        "{}\r\nContent-Length: {}\r\n\r\n{}",
+        status_line,
+        contents.len(),
+        contents
+    );
 
     stream.write(response.as_bytes()).unwrap();
     stream.flush().unwrap();
