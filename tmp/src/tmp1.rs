8c0b1d24168000e73211d83d6d3453cbe01d54e2
aggresss <aggresss@163.com>
2022-06-19 15:20:12 +0800

    2022-06-19 15:20:12 CST W25D0

--- tmp/src/tmp1.rs
+++ tmp/src/tmp1.rs
@@ -1,16 +1,3 @@
-use std::cell::Cell;
-
-#[derive(Debug)]
-struct Person {
-    name: String,
-    age: Cell<usize>,
-}
-
 fn main() {
-    let person = Person { name: "Job Biden".to_string(), age: Cell::new(79) };
-    let person_ref: &Person = &person;
 
-    println!("Age is : {:?}", person_ref);
-    person_ref.age.set(83);
-    println!("Age is: {:?}", person_ref);
 }
