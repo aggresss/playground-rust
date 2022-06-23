d97f9d5ed7d8c153149d961ab95bcd1581d65311
aggresss <aggresss@163.com>
2022-06-23 09:07:40 +0800

    2022-06-23 09:07:40 CST W25D4

--- workbench/cargo-demo/build.rs
+++ workbench/cargo-demo/build.rs
@@ -2,7 +2,7 @@ use std::env;
 use std::fs;
 use std::path::Path;
 
-fn main() {
+fn hello() {
     let out_dir = env::var_os("OUT_DIR").unwrap();
     let dest_path = Path::new(&out_dir).join("hello.rs");
     fs::write(
@@ -12,5 +12,15 @@ fn main() {
         }
         "
     ).unwrap();
+
+}
+
+fn link_z() {
+    pkg_config::Config::new().probe("zlib").unwrap();
+}
+
+fn main() {
+    hello();
+    link_z();
     println!("cargo:rerun-if-changed=build.rs");
 }
--- workbench/cargo-demo/src/lib.rs
+++ workbench/cargo-demo/src/lib.rs
@@ -1,10 +1,13 @@
-// src/lib.rs (default entry point for proc macros)
+use std::os::raw::{c_uint, c_ulong};
 
-extern crate proc_macro; // Apparently needed to be imported like this.
+extern "C" {
+    pub fn crc32(crc: c_ulong, buf: *const u8, len: c_uint) -> c_ulong;
+}
 
-use proc_macro::TokenStream;
-
-#[proc_macro_attribute] // Can now be used as `#[my_attribute]`
-pub fn my_attribute(_attr: TokenStream, item: TokenStream) -> TokenStream {
-    item
+#[test]
+fn test_crc32() {
+    let s = "hello";
+    unsafe {
+        assert_eq!(crc32(0, s.as_ptr(), s.len() as c_uint), 0x3610a686);
+    }
 }
