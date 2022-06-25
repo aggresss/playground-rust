be93220ff1dc5ba6edaa9543a562742320bdc1b5
aggresss <aggresss@163.com>
2022-06-25 21:01:37 +0800

    2022-06-25 21:01:37 CST W25D6

--- tmp/README.md
+++ tmp/README.md
@@ -1,7 +1,20 @@
 
+## Scratch 001
 
 Rust is an `expression-based` language
 
 `Statements` are instructions that perform some action and do not return a value. `Expressions` evaluate to a resulting value.
 
 Calling a function is an expression, Calling a macro is an expression, A new scope block created with curly brackets is an expression.
+
+## Scratch 002
+
+Note that a reference's scope starts from where it is introduced and continues through the last time that reference is used.
+
+The ability of the compiler to tell that a reference is no longer being used at a point before the end of the scope is called Non-Lexical Lifetimes (NLL).
+
+## Scratch 003
+
+- Field Init Shorthand
+- Struct Update Syntax
+
--- tmp/examples/tmp6.rs
+++ tmp/examples/tmp6.rs
@@ -1,6 +1,35 @@
+#![allow(dead_code)]
 
 fn main() {
-    for number in (1..4).rev() {
-        println!("{}", number);
-    }
+    let user1 = User {
+        user: String::from("alpha"),
+        email: String::from("alpha@example.com"),
+        active: true,
+        sign_in_count: 1,
+    };
+
+    // If we had given user2 new String values for both email and username,
+    // and thus only used the active and sign_in_count values from user1,
+    // then user1 would still be valid after creating user2.
+    // The types of active and sign_in_count are types that implement the Copy trait,
+    // so the behavior we discussed in the “Stack-Only Data: Copy” section would apply.
+    let user2 = User {
+        user: String::from("beta"),
+        email: String::from("beta@example.com"),
+        ..user1
+    };
+
+    // let user2 = user1;
+
+    println!("{:?}", user2);
+
+    println!("{:?}", user1);
+}
+
+#[derive(Debug)]
+struct User {
+    active: bool,
+    user: String,
+    email: String,
+    sign_in_count: u64,
 }
