bf515f5973f6a917e61d01a0cf58a1873ede3083
aggresss <aggresss@163.com>
2022-06-28 14:50:08 +0800

    2022-06-28 14:50:08 CST W26D2

--- tmp/examples/tmp7.rs
+++ tmp/examples/tmp7.rs
@@ -1,13 +1,9 @@
+use std::error::Error;
 use std::fs::File;
-use std::io;
-use std::io::Read;
 
-fn read_username_frome_file() -> Result<String, io::Error> {
-    let mut s = String::new();
-    File::open("hello.txt")?.read_to_string(&mut s)?;
-    Ok(s)
+fn main() -> Result<(), Box<dyn Error>>{
+    let _f = File::open("hello.txt")?;
+
+    Ok(())
 }
 
-fn main() {
-    let _ = read_username_frome_file();
-}
