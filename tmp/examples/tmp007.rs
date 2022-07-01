e484842026cd8605a85bfd9fc010ee773dbb0092
aggresss <aggresss@163.com>
2022-07-01 09:40:44 +0800

    2022-07-01 09:40:44 CST W26D5

--- tmp/examples/tmp007.rs
+++ tmp/examples/tmp007.rs
@@ -1,4 +1,28 @@
+#[derive(PartialEq, Debug)]
+struct Shoe {
+    size: u32,
+    style: String,
+}
+
+fn shoes_in_size(shoes: Vec<Shoe>, shoe_size: u32) -> Vec<Shoe> {
+    shoes.into_iter().filter(|s| s.size == shoe_size).collect()
+}
+
+
+
 fn main() {
-    let b = Box::new(5);
-    println!("b = {}", b);
+    let shoes = vec![
+        Shoe {
+            size: 10,
+            style: String::from("sneaker"),
+        },
+        Shoe {
+            size: 13,
+            style: String::from("sandal"),
+        }
+    ];
+
+    let in_my_size = shoes_in_size(shoes, 10);
+
+    println!("{:?}", in_my_size);
 }
