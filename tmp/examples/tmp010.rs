329a31a8725a7aac8f712e8ef48f84635e5aa2cd
aggresss <aggresss@163.com>
2022-07-02 11:16:33 +0800

    2022-07-02 11:16:33 CST W26D6

--- tmp/examples/tmp010.rs
+++ tmp/examples/tmp010.rs
@@ -1,21 +1,20 @@
-struct CustomSmartPointer {
-    data: String,
+enum List {
+    Cons(i32, Rc<List>),
+    Nil,
 }
 
-impl Drop for CustomSmartPointer {
-    fn drop(&mut self) {
-        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
-    }
-}
+use crate::List::{Cons, Nil};
+use std::rc::Rc;
 
 fn main() {
-    let c = CustomSmartPointer {
-        data: String::from("my stuff"),
-    };
+    let a = Rc::new(Cons(5, Rc::new(Cons(10, Rc::new(Nil)))));
+    println!("count after creating a = {}", Rc::strong_count(&a));
+    let _b = Cons(3, Rc::clone(&a));
+    println!("count after creating b = {}", Rc::strong_count(&a));
+    {
+        let _c = Cons(4, Rc::clone(&a));
+        println!("count after creating c = {}", Rc::strong_count(&a));
+    }
 
-    println!("CustomSmartPointer created.");
-
-    drop(c);
-
-    println!("CustomSmartPointer dropped before the end of main.");
+    println!("count after c goes out of scope = {}", Rc::strong_count(&a));
 }
