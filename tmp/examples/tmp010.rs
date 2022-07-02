a59cae8337d0ed4a8038790873df17072342423d
aggresss <aggresss@163.com>
2022-07-02 09:51:14 +0800

    2022-07-02 09:51:14 CST W26D6

--- tmp/examples/tmp010.rs
+++ tmp/examples/tmp010.rs
@@ -1,27 +1,21 @@
-use std::ops::Deref;
-
-struct MyBox<T>(T);
-
-impl<T> MyBox<T> {
-    fn new(x: T) -> MyBox<T> {
-        MyBox(x)
-    }
+struct CustomSmartPointer {
+    data: String,
 }
 
-impl<T> Deref for MyBox<T> {
-    type Target = T;
-
-    fn deref(&self) -> &Self::Target {
-        &self.0
+impl Drop for CustomSmartPointer {
+    fn drop(&mut self) {
+        println!("Dropping CustomSmartPointer with data `{}`!", self.data);
     }
-}
-
-fn hello(name: &str) {
-    println!("Hello, {}!", name);
 }
 
 fn main() {
-    let m = MyBox::new(String::from("Rust"));
-    hello(&m);
-    hello(&(*m)[..]);
+    let _c = CustomSmartPointer {
+        data: String::from("my stuff"),
+    };
+
+    let _d = CustomSmartPointer {
+        data: String::from("other stuff"),
+    };
+
+    println!("CustomSmartPointer created.");
 }
