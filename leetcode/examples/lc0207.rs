f65e569278ea27f9ff5c4f0ccf54510dbb85dc1a
aggresss <aggresss@163.com>
2022-08-14 17:56:06 +0800

    2022-08-14 17:56:06 CST W33D0

---
+++ leetcode/examples/lc0207.rs
@@ -0,0 +1,69 @@
+#![allow(dead_code)]
+
+fn main() {}
+
+struct Solution {}
+
+use std::collections::VecDeque;
+
+impl Solution {
+    pub fn can_finish(num_courses: i32, prerequisites: Vec<Vec<i32>>) -> bool {
+        let num = num_courses as usize;
+        let mut matrix = vec![vec![false; num]; num];
+        let mut in_degree = vec![0; num];
+
+        for pre in prerequisites.iter() {
+            if !matrix[pre[1] as usize][pre[0] as usize] {
+                in_degree[pre[0] as usize] += 1;
+            }
+            matrix[pre[1] as usize][pre[0] as usize] = true;
+        }
+
+        let mut deq = VecDeque::new();
+        for (node, &v) in in_degree.iter().enumerate() {
+            if v == 0 {
+                deq.push_back(node);
+            }
+        }
+
+        let mut cnt = 0;
+        while let Some(node) = deq.pop_front() {
+            cnt += 1;
+            for (i, &connect) in matrix[node].iter().enumerate() {
+                if connect {
+                    in_degree[i] -= 1;
+                    if in_degree[i] == 0 {
+                        deq.push_back(i);
+                    }
+                }
+            }
+        }
+
+        cnt == num_courses
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn tests() {
+        assert_eq!(Solution::can_finish(2, vec![vec![1, 0]]), true);
+        assert_eq!(Solution::can_finish(2, vec![vec![1, 0], vec![0, 1]]), false);
+        assert_eq!(
+            Solution::can_finish(
+                8,
+                vec![
+                    vec![1, 0],
+                    vec![2, 6],
+                    vec![1, 7],
+                    vec![6, 4],
+                    vec![7, 0],
+                    vec![0, 5]
+                ]
+            ),
+            true
+        );
+    }
+}
