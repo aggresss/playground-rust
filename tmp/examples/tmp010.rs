0137f9418bc2928a821dcb9bfae9e2d0cd1bc84c
aggresss <aggresss@163.com>
2022-08-16 10:54:41 +0800

    2022-08-16 10:54:41 CST W33D2

--- tmp/examples/tmp010.rs
+++ tmp/examples/tmp010.rs
@@ -1,15 +1,14 @@
-#![allow(dead_code)]
+use std::{cell::RefCell, cell::RefMut, collections::HashMap, rc::Rc};
 
-fn main() {}
+fn main() {
+    let shared_map: Rc<RefCell<_>> = Rc::new(RefCell::new(HashMap::new()));
 
-struct Solution {}
+    {
+        let mut map: RefMut<_> = shared_map.borrow_mut();
+        map.insert("africa", 92388);
+        map.insert("kyoto", 11837);
+    }
 
-impl Solution {}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[test]
-    fn tests() {}
+    let total: i32 = shared_map.borrow().values().sum();
+    println!("{}", total);
 }
