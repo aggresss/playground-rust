bad2230641a70a01cb8fab606505299b58854225
aggresss <aggresss@163.com>
2022-08-16 11:45:09 +0800

    2022-08-16 11:45:09 CST W33D2

--- tmp/examples/tmp010.rs
+++ tmp/examples/tmp010.rs
@@ -1,14 +1,45 @@
-use std::{cell::RefCell, cell::RefMut, collections::HashMap, rc::Rc};
+use std::{
+    cell::RefCell,
+    rc::Rc, vec,
+};
+
+#[derive(Debug, PartialEq, Eq)]
+pub struct TreeNode {
+    pub val: i32,
+    pub left: Option<Rc<RefCell<TreeNode>>>,
+    pub right: Option<Rc<RefCell<TreeNode>>>,
+}
+
+impl TreeNode {
+    #[inline]
+    pub fn new(val: i32) -> Self {
+        TreeNode {
+            val: val,
+            left: None,
+            right: None,
+        }
+    }
+}
+
+pub fn inorder_traverse(root: Option<Rc<RefCell<TreeNode>>>) -> Vec<i32> {
+    fn dfs(root: Option<Rc<RefCell<TreeNode>>>, list: &mut Vec<i32>) {
+        if let Some(x) = root {
+            dfs(x.borrow().left.clone(), list);
+            list.push(x.borrow().val);
+            dfs(x.borrow().right.clone(), list);
+        }
+    }
+    let mut list: Vec<i32> = vec![];
+    dfs(root, &mut list);
+    list
+}
 
 fn main() {
-    let shared_map: Rc<RefCell<_>> = Rc::new(RefCell::new(HashMap::new()));
-
-    {
-        let mut map: RefMut<_> = shared_map.borrow_mut();
-        map.insert("africa", 92388);
-        map.insert("kyoto", 11837);
-    }
-
-    let total: i32 = shared_map.borrow().values().sum();
-    println!("{}", total);
+    let mut s1: TreeNode = TreeNode::new(2);
+    let s2: TreeNode = TreeNode::new(1);
+    let s3: TreeNode = TreeNode::new(3);
+    s1.left = Some(Rc::new(RefCell::new(s2)));
+    s1.right = Some(Rc::new(RefCell::new(s3)));
+    let root = Some(Rc::new(RefCell::new(s1)));
+    println!("{:?}", inorder_traverse(root));
 }
