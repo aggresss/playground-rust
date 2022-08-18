2855d8d3751f606465bf40f4913a1fc1a74e5fb2
aggresss <aggresss@163.com>
2022-08-18 15:01:29 +0800

    2022-08-18 15:01:29 CST W33D4

--- algorithm/src/graph/breadth_first_search.rs
+++ algorithm/src/graph/breadth_first_search.rs
@@ -92,6 +92,69 @@ mod tests {
         let root = 1;
         let target = 10;
 
+        assert_eq!(
+            breadth_first_search(&graph, root.into(), target.into()),
+            None
+        );
+    }
+
+    #[test]
+    fn breadth_first_search_graph1_when_8_should_evaluate_all_nodes_first() {
+        let graph = graph1();
+        let root = 1;
+        let target = 8;
+
+        let expected_path = vec![1, 2, 3, 4, 5, 6, 7, 8];
+
+        assert_eq!(
+            breadth_first_search(&graph, root.into(), target.into()),
+            Some(expected_path)
+        );
+    }
+
+    fn graph2() -> Graph {
+        let nodes = vec![1, 2, 3, 4, 5, 6, 7, 8];
+        let undirected_edges = vec![
+            (1, 2),
+            (2, 1),
+            (2, 5),
+            (5, 2),
+            (2, 6),
+            (6, 2),
+            (3, 4),
+            (4, 3),
+            (3, 6),
+            (6, 3),
+            (4, 7),
+            (7, 4),
+            (6, 7),
+            (7, 6),
+        ];
+
+        Graph::new(
+            nodes.into_iter().map(|v| v.into()).collect(),
+            undirected_edges.into_iter().map(|e| e.into()).collect(),
+        )
+    }
+
+    #[test]
+    fn breadth_first_search_graph2_when_no_path_to_node_returns_none() {
+        let graph = graph2();
+        let root = 8;
+        let target = 4;
+
         assert_eq!(breadth_first_search(&graph, root.into(), target.into()), None);
+    }
+
+    #[test]
+    fn breadth_first_search_graph2_should_find_path_from_4_to_1() {
+        let graph = graph2();
+
+        let root = 4;
+        let target = 1;
+
+        let expected_path = vec![4, 3,7,6,2,1];
+
+        assert_eq!(breadth_first_search(&graph, root.into(), target.into()), Some(expected_path));
     }
 }
