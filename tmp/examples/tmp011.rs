2256721acbf2a5149a1ecca2142e6c4d4248e9c8
aggresss <aggresss@163.com>
2022-09-21 08:23:24 +0800

    2022-09-21 08:23:24 CST W38D3

--- tmp/examples/tmp011.rs
+++ tmp/examples/tmp011.rs
@@ -1,92 +1,37 @@
-#![allow(dead_code)]
+use std::{collections::{BTreeMap, BinaryHeap}, cmp::Reverse, ops::Add};
 
-fn main() {
-    let mut dsu = DisjointSetUnion::new(10);
-    let edges: Vec<(usize, usize)> = vec![
-        (1, 2),
-        (2, 1),
-        (2, 3),
-        (1, 3),
-        (4, 5),
-        (7, 8),
-        (4, 8),
-        (3, 8),
-        (1, 9),
-        (2, 9),
-        (3, 9),
-        (4, 9),
-        (5, 9),
-        (6, 9),
-        (7, 9),
-    ];
-    let expected_edges: Vec<(usize, usize)> = vec![
-        (1, 2),
-        (2, 3),
-        (4, 5),
-        (7, 8),
-        (4, 8),
-        (3, 8),
-        (1, 9),
-        (6, 9),
-    ];
-    let mut added_edges: Vec<(usize, usize)> = Vec::new();
-    for (u, v) in edges {
-        if dsu.merge(u, v) < std::usize::MAX {
-            added_edges.push((u, v));
-        }
-        assert!(dsu.merge(u, v) == std::usize::MAX);
-    }
-    assert_eq!(added_edges, expected_edges);
-    let comp_1 = dsu.find_set(1);
-    for i in 2..=9 {
-        assert_eq!(comp_1, dsu.find_set(i));
-    }
-    assert_ne!(comp_1, dsu.find_set(0));
-}
 
-pub struct DSUNode {
-    parent: usize,
-    size: usize,
-}
+fn main() {}
 
-pub struct DisjointSetUnion {
-    nodes: Vec<DSUNode>,
-}
+type Graph<V, E> = BTreeMap<V, BTreeMap<V, E>>;
 
-impl DisjointSetUnion {
-    pub fn new(n: usize) -> DisjointSetUnion {
-        let mut nodes = Vec::new();
-        nodes.reserve_exact(n);
-        for i in 0..n {
-            nodes.push(DSUNode { parent: i, size: 1 });
-        }
-        DisjointSetUnion { nodes }
+pub fn dijkstra<V, E>(graph: &Graph<V, E>, start: &V) -> BTreeMap<V, Option<V, E>>
+where
+    V: Ord + Copy,
+    E: Ord + Copy + Add<Output = E>
+{
+    let mut ans = BTreeMap::new();
+    let mut prio = BinaryHeap::new();
+
+    ans.insert(*start, None);
+
+    for (new, new_weight) in &graph[start] {
+        ans.insert(*new, Some((*start, *new_weight)));
+        prio.push(Reverse((*new_weight, new, start)));
     }
 
-    pub fn find_set(&self, v: usize) -> usize {
-        let mut p = v;
-        while p != self.nodes[p].parent {
-            p = self.nodes[p].parent
+    while let Some(Reverse((dist_new, new, prev))) = prio.pop() {
+        for (next, next_weight) in &graph[new] {
+            match ans.get(next) {
+                Some(Some((_, dist_next))) if dist_new + next_weight >= dist_next => {}
+                Some(None) => {}
+                _ => {
+                    ans.insert(*next, Some((*new, dist_new + *next_weight)));
+                    prio.push(Reverse((dist_new + *next_weight, next, new)));
+                }
+            }
         }
-
-        p
     }
 
-    pub fn merge(&mut self, u: usize, v: usize) -> usize {
-        let mut a = self.find_set(u);
-        let mut b = self.find_set(v);
-
-        if a == b {
-            return std::usize::MAX;
-        }
-
-        if self.nodes[a].size < self.nodes[b].size {
-            std::mem::swap(&mut a, &mut b);
-        }
-
-        self.nodes[b].parent = a;
-        self.nodes[a].size += self.nodes[b].size;
-
-        a
-    }
+    ans
 }
