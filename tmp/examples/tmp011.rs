536055a43927f509968fd7c39af8fc867ee02700
aggresss <aggresss@163.com>
2022-09-22 08:38:48 +0800

    2022-09-22 08:38:48 CST W38D4

--- tmp/examples/tmp011.rs
+++ tmp/examples/tmp011.rs
@@ -1,33 +1,31 @@
-use std::{collections::{BTreeMap, BinaryHeap}, cmp::Reverse, ops::Add};
-
+use std::{collections::{BTreeMap, BinaryHeap}, ops::Add, cmp::Reverse};
 
 fn main() {}
 
 type Graph<V, E> = BTreeMap<V, BTreeMap<V, E>>;
 
-pub fn dijkstra<V, E>(graph: &Graph<V, E>, start: &V) -> BTreeMap<V, Option<V, E>>
+pub fn dijkstra<V, E>(graph: &Graph<V, E>, start: &V) -> BTreeMap<V, Option<(V, E)>>
 where
     V: Ord + Copy,
-    E: Ord + Copy + Add<Output = E>
+    E: Ord + Copy + Add<Output = E>,
 {
-    let mut ans = BTreeMap::new();
     let mut prio = BinaryHeap::new();
+    let mut ans = BTreeMap::new();
 
     ans.insert(*start, None);
-
-    for (new, new_weight) in &graph[start] {
-        ans.insert(*new, Some((*start, *new_weight)));
-        prio.push(Reverse((*new_weight, new, start)));
+    for (new, weight) in &graph[start] {
+        ans.insert(*new, Some((*start, *weight)));
+        prio.push(Reverse((*weight, new, start)));
     }
 
-    while let Some(Reverse((dist_new, new, prev))) = prio.pop() {
-        for (next, next_weight) in &graph[new] {
+    while let Some(Reverse((dist_new, new, _))) = prio.pop() {
+        for (next, weight) in &graph[new] {
             match ans.get(next) {
-                Some(Some((_, dist_next))) if dist_new + next_weight >= dist_next => {}
+                Some(Some((_, next_dist))) if dist_new + *weight >= *next_dist => {}
                 Some(None) => {}
                 _ => {
-                    ans.insert(*next, Some((*new, dist_new + *next_weight)));
-                    prio.push(Reverse((dist_new + *next_weight, next, new)));
+                    prio.push(Reverse((dist_new + *weight, next, new)));
+                    ans.insert(*next, Some((*new, dist_new + *weight)));
                 }
             }
         }
@@ -35,3 +33,4 @@ where
 
     ans
 }
+
