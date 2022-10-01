75b7cb75076f66ef148ff9c340f40aefa1e00e8b
aggresss <aggresss@163.com>
2022-10-01 21:37:09 +0800

    2022-10-01 21:37:09 CST W39D6

--- algorithm/src/sorting/insertion_sort.rs
+++ algorithm/src/sorting/insertion_sort.rs
@@ -1,4 +1,3 @@
-
 pub fn insertion_sort<T>(arr: &mut [T])
 where
     T: PartialOrd + Copy,
@@ -30,6 +29,41 @@ mod tests {
     #[test]
     fn empty() {
         let mut arr: [u8; 0] = [];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn one_element() {
+        let mut arr: [char; 1] = ['a'];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn already_sorted() {
+        let mut arr: [&str; 4] = ["a", "b", "c"];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn basic() {
+        let mut arr: [&str; 4] = ["d", "a", "c", "b"];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn odd_number_of_elements() {
+        let mut arr: Vec<&str> = vec!["d", "a", "c", "e", "b"];
+        insertion_sort(&mut arr);
+        assert!(is_sorted(&arr));
+    }
+
+    #[test]
+    fn repeated_elements() {
+        let mut arr: Vec<usize> = vec![542, 542, 542, 542];
         insertion_sort(&mut arr);
         assert!(is_sorted(&arr));
     }
