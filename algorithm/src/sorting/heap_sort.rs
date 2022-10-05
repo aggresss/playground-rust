5731fba22f4178424e86c3e48268cc17bf472965
aggresss <aggresss@163.com>
2022-10-05 20:20:05 +0800

    2022-10-05 20:20:05 CST W40D3

--- algorithm/src/sorting/heap_sort.rs
+++ algorithm/src/sorting/heap_sort.rs
@@ -38,3 +38,50 @@ fn move_down<T: Ord>(arr: &mut [T], mut root: usize) {
         root = max;
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn empty() {
+        let mut arr: Vec<i32> = Vec::new();
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[]);
+    }
+
+    #[test]
+    fn single_element() {
+        let mut arr = vec![1];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[1]);
+    }
+
+    #[test]
+    fn sorted_array() {
+        let mut arr = vec![1, 2, 3, 4];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn unsorted_array() {
+        let mut arr = vec![3, 4, 2, 1];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[1, 2, 3, 4]);
+    }
+
+    #[test]
+    fn odd_number_of_elements() {
+        let mut arr = vec![3, 4, 2, 1, 7];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &[1, 2, 3, 4, 7]);
+    }
+
+    #[test]
+    fn repeated_elements() {
+        let mut arr = vec![542, 542, 542, 542];
+        heap_sort(&mut arr);
+        assert_eq!(&arr, &vec![542, 542, 542, 542]);
+    }
+}
