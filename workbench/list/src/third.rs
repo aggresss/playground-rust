6e40ac6b51844740159666444ae0f0e41019d681
aggresss <aggresss@163.com>
2022-12-15 15:26:10 +0800

    2022-12-15 15:26:10 CST W50D4

--- workbench/list/src/third.rs
+++ workbench/list/src/third.rs
@@ -2,13 +2,13 @@ use std::rc::Rc;
 
 type Link<T> = Option<Rc<Node<T>>>;
 
-pub struct List<T> {
-    head: Link<T>,
-}
-
 struct Node<T> {
     elem: T,
     next: Link<T>,
+}
+
+pub struct List<T> {
+    head: Link<T>,
 }
 
 impl<T> List<T> {
