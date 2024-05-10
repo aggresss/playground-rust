8556b68a6e5bde754f648e58a8693e72d972ede5
aggresss <aggresss@163.com>
2024-05-10 09:09:35 +0800

    2024-05-10 09:09:35 CST W18D5

--- workbench/std-demo/examples/007_arc_hashmap.rs
+++ workbench/std-demo/examples/007_arc_hashmap.rs
@@ -1,18 +1,20 @@
 /*
-https://stackoverflow.com/questions/50282619/is-it-possible-to-share-a-hashmap-between-threads-without-locking-the-entire-has
-https://nickymeuleman.netlify.app/blog/multithreading-rust
+- https://stackoverflow.com/questions/50282619/is-it-possible-to-share-a-hashmap-between-threads-without-locking-the-entire-has
+- https://nickymeuleman.netlify.app/blog/multithreading-rust
+- https://www.jb51.net/program/317685aja.htm
 
 */
 
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex, RwLock},
+    sync::{Arc, RwLock},
     thread,
     time::Duration,
 };
 
 fn main() {
-    let data = Arc::new(RwLock::new(HashMap::new()));
+    let data: Arc<RwLock<HashMap<u8, i32>>> =
+        Arc::new(RwLock::new(vec![(4, 9), (5, 8)].into_iter().collect()));
 
     let threads: Vec<_> = (0..10)
         .map(|i| {
@@ -28,21 +30,13 @@ fn main() {
     println!("{:?}", data);
 }
 
-fn worker_thread(id: u8, data: Arc<RwLock<HashMap<u8, Mutex<i32>>>>) {
+fn worker_thread(id: u8, data: Arc<RwLock<HashMap<u8, i32>>>) {
     loop {
         {
-            // Assume that the element already exists
-            let map = data.read().expect("RwLock poisoned");
-
-            if let Some(element) = map.get(&id) {
-                let mut element = element.lock().expect("Mutex poisoned");
-
-                // Perform our normal work updating a specific element.
-                // The entire HashMap only has a read lock, which
-                // means that other threads can access it.
-                *element += 1;
+            let mut map = data.write().expect("RwLock poisoned");
+            if let Some(_) = map.get(&id) {
+                map.entry(id).and_modify(|v| *v += 100);
                 thread::sleep(Duration::from_millis(50));
-
                 return;
             }
         }
@@ -51,12 +45,8 @@ fn worker_thread(id: u8, data: Arc<RwLock<HashMap<u8, Mutex<i32>>>>) {
 
         {
             let mut map = data.write().expect("RwLock poisoned");
-
-            // We use HashMap::entry to handle the case where another thread
-            // inserted the same key while where were unlocked.
             thread::sleep(Duration::from_millis(50));
-            map.entry(id).or_insert_with(|| Mutex::new(0));
-            // Let the loop start us over to try again
+            map.entry(id).or_insert(0);
         }
     }
 }
